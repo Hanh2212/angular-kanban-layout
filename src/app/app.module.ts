a9563545bf3e4dd71a5c2fdf4e27dd6cea97ab2c
HanhDo <dthhanhptit@gmail.com>
2022-03-08 11:09:57 +0700

    update: layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { MainViewComponent } from './pages/main-view/main-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/board.model.ts
@@ -0,0 +1,6 @@
+import './column.model';
+import { Column } from './column.model';
+
+export class Board {
+  constructor(public name: string, public columns: Column[]){}
+}
